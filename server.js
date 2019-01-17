aabe6f12eea55de72c28091794c0b1911d38a7a4
Jacob Selbe <jalxselbe@gmail.com>
2019-01-17 00:05:07 -0600

    server skeleton created

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = require()
+const express = require('express');
+const morgan = require('morgan');
+const jsonParser = require('body-parser').json();
+const {BlogPosts} = require('./models');
+// const router = express.Router(); //uncomment if modularized
+const app = express();
+
+app.use(morgan('common'));
+
+app.get();
+
+app.post();
+
+app.put();
+
+app.delete();
+
+app.listen();
